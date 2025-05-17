60ea263eb15a094612102b9afa2130d89165efd4
Owen-FTC <27orinne@gmail.com>
2025-05-17 18:58:24 -0400

    Improvements

--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
-def split_table(flat_list):
+def split_table(flat_list: tuple) -> tuple:
     """Converts a flat list of 64 numbers into an 8x8 2D list."""
-    return [flat_list[i * 8:(i + 1) * 8] for i in range(8)]
+    return tuple([flat_list[i * 8:(i + 1) * 8] for i in range(8)])
 
 
-def flatten(board):
-    return [x for row in board for x in row]
+def flatten(board) -> tuple:
+    return tuple([x for row in board for x in row])
 
 # For black pieces, we mirror the white table vertically.
-def mirror(table, flattened=True):
+def mirror(table: tuple, flattened=True) -> tuple:
     """Mirrors an 8x8 table vertically."""
     if flattened:
         return flatten(split_table(table)[::-1])
     return table[::-1]
 
-def negate(lst):
-    return [-x for x in lst]
+def negate(lst: tuple[int, ...]) -> tuple[int, ...]:
+    return tuple([-x for x in lst])
 
