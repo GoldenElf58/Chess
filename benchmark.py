76ad9925eaa07fdef3546930e3790d31e3fa65e5
Owen-FTC <27orinne@gmail.com>
2025-06-09 13:33:51 -0400

    benchmark sig figs and units updated

--- benchmark.py
+++ benchmark.py
@@ -42,15 +42,15 @@ def benchmark(condition: bool, game_state: GameStateV3) -> None:
     #     game_state.move(bot.generate_move(game_state, depth=2)[0][1])
 
     if condition:
-        game_state.get_moves_new()
-    else:
         game_state.get_moves()
+    else:
+        game_state.get_moves_no_check()
     # game_state.get_moves()
     # game_state.get_moves_no_check()
     # game_state.move(random.choice(moves))
     # game_state.get_moves_no_check()
     # while game_state.get_winner() is None:
-    #     game_state.move(random.choice(game_state.get_moves()))
+    #     game_state = game_state.move(random.choice(game_state.get_moves()))
     #     game_state.get_moves()
     # BotV1().generate_move(game_state, depth=2)
     # for move in game_state.get_moves():
@@ -62,32 +62,32 @@ def test():
     game_state = GameStateV3()
     t0 = time.time_ns()
     moves = 0
-    bot = BotV1()
+    # bot = BotV1()
     # for i in range(7):
     #     game_state.get_winner()
     while game_state.get_winner() is None:
-        game_state = game_state.move(bot.generate_move(game_state, depth=2)[0][1])
-        game_state.get_moves()
+        game_state = game_state.move(random.choice(game_state.get_moves_no_check()))#game_state.move(bot.generate_move(game_state, depth=2)[0][1])
+        # game_state.get_moves()
         moves += 1
     t1 = time.time_ns()
-    # print(f'Total time (ms): {(t1 - t0) / 1_000_000:.5f}')
-    print(f'Moves: {moves}')
-    print(f'Average time (µs): {((t1 - t0) / 1_000) / moves:.5f}')
-    return moves, (t1 - t0) / moves
+    print(f'Total time (µs): {(t1 - t0) / 1_000:#.5g}')
+    # print(f'Moves: {moves}')
+    print(f'Average time (µs): {((t1 - t0) / 1_000) / moves:#.5g}')
+    return (t1 - t0), (t1 - t0) / moves
 
 def deep_test():
-    moves = []
+    game_times = []
     turn_times = []
     for i in range(100):
-        turns, turn_time = test()
-        moves.append(turns)
+        game_time, turn_time = test()
+        game_times.append(game_time)
         turn_times.append(turn_time)
     print()
-    print(f'Average moves: {mean(moves):.1f}')
-    print(f'Std dev moves: {stdev(moves):.2f}')
+    print(f'Average game time (µs): {mean(game_times) / 1_000:#.5g}')
+    print(f'Std dev game time (µs): {stdev(game_times) / 1_000:#.5g}')
     print()
-    print(f'Average turn time (µs): {mean(turn_times) / 1_000:.5f}')
-    print(f'Std dev turn time (µs): {stdev(turn_times) / 1_000:.5f}')
+    print(f'Average turn time (µs): {mean(turn_times) / 1_000:#.5g}')
+    print(f'Std dev turn time (µs): {stdev(turn_times) / 1_000:#.5g}')
 
 
 def main() -> None:
@@ -96,16 +96,18 @@ def main() -> None:
     t3 = []
     for _ in range(50_000_000): pass
     print('Warmup complete')
-    N = 5000
-    n = 30
+    N = 10000
+    n = 50
     timeit(lambda: benchmark(True, GameStateV3()), number=n*3)
     test = timeit(lambda: benchmark(True, GameStateV3()), number=n) / n
     scale = 1_000_000 if test < .001 else (1_000 if test < 1 else 1)
+    unit = 'µs' if test < .001 else ('ms' if test < 1 else 's')
     print(f'Test: {test}')
     print(f'Scale: {scale:,}')
+    print(f'Unit: {unit}')
     start = random.randint(0, len(game_states) - 1)
     for i in range(start, start + N):
-        game_state = game_states[i % 500]#.copy()
+        game_state = GameState()#game_states[i % 500]#.copy()
 
         # timeit(lambda: benchmark(True, game_state_a), number=500) * 1_000
         # game_state_a.get_moves()
@@ -121,29 +123,28 @@ def main() -> None:
         if (i - start) > 0 and (i - start) % (N // 50) == 0:
             t, p = stats.ttest_1samp(t3, 0, alternative='less')
             # t, p = stats.ttest_ind(t1, t2, equal_var=False, alternative='less')
-            print(f'{(i - start)}:\nNew: {mean(t1)}, {stdev(t1)}')
-            print(f'Old: {mean(t2)}, {stdev(t2)}\n')
-            print(f'T-statistic: {t:.5f}\nP-value: {p:.5f}\n')
+            print(f'{(i - start)}:')
+            print(f'New: {mean(t1):#.5g}{unit}, {stdev(t1):#.5g}{unit}')
+            print(f'Old: {mean(t2):#.5g}{unit}, {stdev(t2):#.5g}{unit}\n')
+            print(f'T-statistic: {t:#.5g}\nP-value: {p:#.5g}\n')
     t1 = t1[N // 100:]
     t2 = t2[N // 100:]
     t3 = t3[N // 100:]
     print(f'{t1}\n{t2}\n{t3}')
     print()
-    print(f'Scale: {scale:,}')
-    print()
-    print(f'Mean New: {mean(t1)}\nStdDev New: {stdev(t1)}\nN New: {len(t1)}\n')
-    print(f'Mean Old: {mean(t2)}\nStdDev Old: {stdev(t2)}\nN Old: {len(t2)}\n')
-    print(f'Mean Diff: {mean(t3)}\nStdDev Diff: {stdev(t3)}\nN Diff: {len(t3)}')
+    print(f'Mean New: {mean(t1):#.5g}{unit}\nStdDev New: {stdev(t1):#.5g}{unit}\nN New: {len(t1):}\n')
+    print(f'Mean Old: {mean(t2):#.5g}{unit}\nStdDev Old: {stdev(t2):#.5g}{unit}\nN Old: {len(t2)}\n')
+    print(f'Mean Diff: {mean(t3):#.5g}{unit}\nStdDev Diff: {stdev(t3):#.5g}{unit}\nN Diff: {len(t3)}')
     print()
     t, p = stats.ttest_ind(t1, t2, equal_var=False, alternative='less')
-    print(f'2-Sample T-Test:\nT-statistic: {t:.5f}\nP-value: {p:.5f}\n')
+    print(f'2-Sample T-Test:\nT-statistic: {t:#.5g}\nP-value: {p:#.5g}\n')
     t, p = stats.ttest_1samp(t3, 0, alternative='less')
-    print(f'1-Sample T-Test of Difference:\nT-statistic: {t:.5f}\nP-value: {p:.5f}')
+    print(f'1-Sample T-Test of Difference:\nT-statistic: {t:#.5g}\nP-value: {p:#.5g}')
     print()
-    print(f'Speedup: {mean(t2) / mean(t1):.5f}')
-    print(f'Slowdown: {mean(t1) / mean(t2):.5f}')
+    print(f'Speedup: {mean(t2) / mean(t1):#.5g}')
+    print(f'Slowdown: {mean(t1) / mean(t2):#.5g}')
 
 
 if __name__ == '__main__':
-    # deep_test()
-    main()
+    deep_test()
+    # main()
